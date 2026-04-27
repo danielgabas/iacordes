a7dc105bb24508d364053bc5cd33a034b15880fc
danielgabas <79529809+danielgabas@users.noreply.github.com>
2026-04-27 13:03:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // Si cambiás algo en index.html, subí el número de CACHE_VERSION para que el
 // celular descargue la nueva versión.
 
-const CACHE_VERSION = 'iacordes-v3';
+const CACHE_VERSION = 'iacordes-v4';
 const CORE_ASSETS = [
   './',
   './index.html',
